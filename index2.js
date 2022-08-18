82ba6c4d1fb7e1cc4a5b8f364cf0e24bb3590a50
dante tabo <dantetabogmail.com>
2022-08-17 22:07:56 -0300

    Primer Entrega

---
+++ index2.js
@@ -0,0 +1,55 @@
+class Persona {
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = [];
+        this.mascotas = [];
+    }
+
+    getFullName() {
+       return(`El completo es: ${this.nombre} ${this.apellido}.`) 
+    }
+
+    addMascotas(mascotas) {
+        this.mascotas.push(mascotas);
+    }
+
+    countMascotas(){
+           return (this.mascotas.length)
+    }
+
+    addBook(nombre, autor){
+        this.libros.push ({nombre, autor})
+        
+    }
+
+    getBookName() {
+
+            return this.libros.map(object =>{
+                return (object.autor);
+            }); 
+
+        //return this.libros.filter(autor => autor.nombre === autor)
+        
+    }
+}
+
+//
+const persona1 = new Persona("Damian", "Chaqueta",);
+const persona2 = new Persona("Santiago", "Merlini");
+
+//mascotas
+persona1.addMascotas("Merlo");
+persona1.addMascotas("Dante");
+persona1.addMascotas("Mario");
+
+//libros
+persona1.addBook("El Poder de los hábitos", "Charles Duhigg")
+persona1.addBook("Focus", "Daniel Goleman")
+
+//consols
+console.log(persona1.getFullName())
+console.log(persona1.mascotas)
+console.log(persona1.countMascotas())
+console.log(persona1.libros)
+console.log(persona1.getBookName())
